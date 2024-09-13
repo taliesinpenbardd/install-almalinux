de4231536ad1458993d9d50e55fc0688ec4fd24f
Luc Delaborde <lucdelaborde@gmail.com>
2024-09-13 15:01:47 +0200

    make the sh script more readable

--- install-almalinux.sh
+++ install-almalinux.sh
@@ -116,6 +116,7 @@ echo "************************************************************"
 
 sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
+
 echo "Done."
 
 #############################################
@@ -126,64 +127,81 @@ echo "Done."
 echo "************************************************************"
 echo "Updating the system..."
 echo "************************************************************"
+
 sudo dnf update -y
+
 echo "Done."
 
 # Install tar
 echo "************************************************************"
 echo "Installing tar..."
 echo "************************************************************"
+
 sudo dnf install tar -y
+
 echo "Done."
 
 # Install epel-release
 echo "************************************************************"
 echo "Installing epel-release..."
 echo "************************************************************"
+
 sudo dnf install epel-release -y
 sudo dnf update -y
+
 echo "Done."
 
 # Install Git
 echo "************************************************************"
 echo "Installing Git..."
 echo "************************************************************"
+
 sudo dnf install git -y
+
 echo "Done."
 
 # Install curl
 echo "************************************************************"
 echo "Installing curl..."
 echo "************************************************************"
+
 sudo dnf install curl -y
+
 echo "Done."
 
 # Install micro editor
 echo "************************************************************"
 echo "Installing micro editor..."
 echo "************************************************************"
+
 sudo curl https://getmic.ro | bash
 sudo mv micro /usr/bin
+
 echo "Done."
 
 # Install docker
 echo "************************************************************"
 echo "Installing docker..."
 echo "************************************************************"
+
 sudo dnf install docker -y
+
 echo "Done."
 
 # Install NodeJS
 echo "************************************************************"
-echo "Installing NodeJS 18"
+echo "Installing NodeJS 20"
 echo "************************************************************"
-sudo dnf module install nodejs:18 -y
+
+sudo dnf module install nodejs:20 -y
+
 echo "Done."
 
 # Install PHP-FPM
 echo "************************************************************"
 echo "Installing PHP-FPM..."
 echo "************************************************************"
+
 sudo dnf install -y http://rpms.remirepo.net/enterprise/remi-release-9.rpm
 sudo dnf makecache -y
 sudo dnf module reset php -y
@@ -196,19 +214,23 @@ sudo sed -i 's/;listen.mode = 0660/listen.mode = 0660/g' /etc/php-fpm.d/www.conf
 sudo sed -i 's/listen.acl_users = apache,nginx/listen.acl_users = apache,nginx,caddy/g' /etc/php-fpm.d/www.conf
 sudo systemctl enable php-fpm
 # sudo systemctl start php-fpm
+
 echo "Done."
 
 # Install Composer
 echo "************************************************************"
 echo "Installing Composer..."
 echo "************************************************************"
+
 sudo dnf install composer -y
+
 echo "Done."
 
 # Install caddy server
 echo "************************************************************"
 echo "Installing caddy server..."
 echo "************************************************************"
+
 sudo dnf install caddy -y
 sudo systemctl enable caddy
 sudo mkdir -p /var/www/production/html
@@ -217,42 +239,50 @@ sudo touch /var/www/production/logs/access.caddy.log
 sudo cp localhost.caddyfile /etc/caddy/Caddyfile.d
 sudo systemctl start caddy
 sudo systemctl start php-fpm
+
 echo "Done."
 
 # Enable and start firewalld
 echo "************************************************************"
 echo "Enabling and starting firewalld..."
 echo "************************************************************"
+
 sudo dnf install firewalld -y
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
+
 echo "Done."
 
 # Open ports
 echo "************************************************************"
 echo "Opening ports..."
 echo "************************************************************"
+
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --permanent --add-service=ssh
 # Uncomment the next line if you need your mysql server accessible from outside
 # sudo firewall-cmd --permanent --add-service=mysql
 sudo systemctl restart firewalld
+
 echo "Done."
 
 # Install fail2ban
 echo "************************************************************"
 echo "Installing fail2ban..."
 echo "************************************************************"
+
 sudo dnf install fail2ban -y
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
+
 echo "Done."
 
 # Permissions
 echo "************************************************************"
 echo "Setting permissions..."
 echo "************************************************************"
+
 sudo usermod -aG wheel caddy
 sudo usermod -aG caddy almalinux
 sudo usermod -aG almalinux caddy
@@ -263,14 +293,17 @@ sudo chown -R almalinux:caddy /var/www
 sudo semanage fcontext -a -t httpd_sys_rw_content_t "/var/www/production/html/storage(/.*)?"
 sudo semanage fcontext -a -t httpd_sys_rw_content_t "/var/www/production/html/bootstrap/cache(/.*)?"
 sudo restorecon -Rv /var/www/production/html
+
 echo "Done."
 
 # Install MariaDB
 echo "************************************************************"
 echo "Installing MariaDB..."
 echo "************************************************************"
+
 sudo dnf install mariadb-server -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 sudo mysql_secure_installation
+
 echo "Done."
